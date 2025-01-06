4e6b6a79da9877cf40086d00377e834758f80af7
Jayodpasindu <88393998+Hashith00@users.noreply.github.com>
2025-01-06 10:33:29 +0530

    Added the google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Toaster />
         </body>
       </ClerkProvider>
+      <GoogleAnalytics gaId="G-XXXXXXXXXXXX" />
     </html>
   );
 }
